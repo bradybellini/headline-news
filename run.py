561ac13a778c68c3aba1264f4a31ada30eda8563
Brady Bellini <bbellini@pm.me>
2022-02-25 20:04:53 -0800

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,14 @@
+import logging
 from headlinenews import SupaPSQL
 
 
 def main():
+    logger = logging.getLogger("rogger_logger.run.main")
+    logger.info("Pull Started")
     supa = SupaPSQL()
     supa.run()
+    logger.info("Pull Ended")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
