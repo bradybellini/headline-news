f268cc201f028db45fb9bcbc24bf278e69729851
Brady Bellini <bbellini@pm.me>
2022-02-25 20:04:59 -0800

    Update rssparser.py

--- headlinenews/rssparser.py
+++ headlinenews/rssparser.py
@@ -2,33 +2,39 @@ from io import BytesIO
 from feedparser import parse
 from lxml import html
 from lxml.html.clean import clean_html
-import hashlib
-import time
-import requests
+import hashlib, time, requests, logging
 
 
 class RSSParser:
     def __init__(self) -> None:
-        pass
+        self.logger = logging.getLogger("rogger_logger.rssparser.RSSParser")
+        self.logger.info("Creating an instance of RSSParser")
 
     def _get_article_info(self, feed_id: str, article: dict) -> dict:
         _title = getattr(article, "title", None)
         _link = getattr(article, "link", None)
-        if not _title or not _link:
+        _date = getattr(article, "published", None)
+        if not _title or not _link or not _date:
+            self.logger.warning(
+                f"No Title, Link or Published in feed {feed_id} and article {article}"
+            )
+            self.logger.debug(
+                f"Feed: {feed_id} Article: {article} Title: {_title} Link: {_link} Date: {_date}"
+            )
             return
         summary = getattr(article, "summary", None)
         date = getattr(article, "published", None)
         guidislink = getattr(article, "guidislink", False)
         guid = getattr(article, "guid", None)
-
+        self.logger.debug(f"Getting article info from {feed_id} {article}")
         return {
             "feed_id": feed_id,
             "title": article.title,
             "link": article.link,
             "author": getattr(article, "author", "No Authors Listed"),
-            "published": time.strftime("%Y-%m-%d %H:%M:%S", article["published_parsed"])
-            if date
-            else None,
+            "published": time.strftime(
+                "%Y-%m-%d %H:%M:%S", article["published_parsed"]
+            ),
             "summary": clean_html(html.fromstring(summary)).text_content().strip()
             if summary
             else summary,
@@ -38,15 +44,21 @@ class RSSParser:
 
     def articles(self, feed: str, feed_id: str) -> list:
         try:
+            self.logger.info(f"Fetching feed {feed}")
             r = requests.get(feed, timeout=(30, 30))
             if r.status_code != requests.codes.ok:
-                print(f"Status Code problem for {feed} | Status Code: {r.status_code}")
+                self.logger.warning(
+                    f"Status Code problem for {feed} | Status Code: {r.status_code}"
+                )
                 return
+            self.logger.info(f"Fetched feed {feed}")
         except requests.ReadTimeout:
-            print(f"Timeout when reading {feed} | Status Code: {r.status_code}")
-
+            self.logger.exception("Exception has occurred")
+        self.logger.info(f"Loading content")
         content = BytesIO(r.content)
-
+        self.logger.info(f"Loaded content")
+        self.logger.info(f"Parsing content")
         parsed = parse(content)
+        self.logger.info(f"Parsed content")
         articles = [parsed for parsed in parsed.entries]
         return [self._get_article_info(feed_id, entry) for entry in articles]
